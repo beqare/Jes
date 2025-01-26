9b28be72214e1fc5462e2bf4432d5ffd98c69ce7
beqare <turtelmaster11@gmail.com>
2025-01-26 02:20:17 +0100

    pjoji

--- publish.bat
+++ publish.bat
@@ -1,3 +1,2 @@
-@echo off
 dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:IncludeAllContentForSelfExtract=true /p:TrimMode=Link
 explorer C:\Dev\dotnet\Jes\bin\Release\net9.0\win-x64\publish
